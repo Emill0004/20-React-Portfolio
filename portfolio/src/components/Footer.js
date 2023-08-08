815141143b668cb3caaab2255111d64e7046db00
Eric Miller <e.s.miller13125@gmail.com>
2023-08-08 02:18:58 -0600

    updated footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,8 +4,17 @@ import { Container, Row, Col } from 'react-bootstrap';
 function Footer() {
     return (
         <Container fluid style={{background: '#220C10', color: '#BAC7BE', height: '10vh', position: 'absolute', bottom: '0'}}>
-            <h5>Email: e.s.miller13125@gmail.com</h5>
-            <h5>Phone Number: (801)554-8423</h5>
+            <Row xs={3}>
+                <Col xs={1}>
+                <h5><a href="https://github.com/Emill0004" style={{color: '#BAC7BE'}}>GitHub</a></h5>
+                </Col>
+                <Col xs={1}>
+                <h5><h5><a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" style={{color: '#BAC7BE'}}>Linkedin</a></h5></h5>
+                </Col>
+                <Col xs={1}>
+                <h5><a href="stackoverflow.com" style={{color: '#BAC7BE'}}>stackoverflow</a></h5>
+                </Col>
+            </Row>
         </Container>
     )
 }
